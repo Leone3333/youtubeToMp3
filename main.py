4fdc5b36aeb643d8786f0c3eb848f9e49db3d8b9
leone3333 <leonegoveia1@gmail.com>
2025-02-20 20:56:25 -0300

    roda usando variavel de ambiente

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template,jsonify
 from conversor import download_audio 
 
@@ -28,4 +29,6 @@ def converter():
         return jsonify({"error": f"Erro no servidor: {str(e)}"}),500
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)   
     app.run(debug=True)
