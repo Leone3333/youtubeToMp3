9eeced9c6144da61fba5dfecd5c13d898c01b693
leone3333 <leonegoveia1@gmail.com>
2025-02-23 09:32:00 -0300

    enviar mp3 para o navegador 1

--- conversor.py
+++ conversor.py
@@ -1,20 +1,23 @@
 import os
 import base64
 import yt_dlp
-from flask import Flask, jsonify, send_file, request   
+from flask import Flask, jsonify, send_file, request, safe_join
 
-# output_directory = r"C:\Users\Desktop\Documents\Rockstar Games\GTA V\User Music"
-# musics teste: https://youtu.be/ZyfrHUV6nH0?si=HgcWDv_4Oz0nnyQc 
-# https://youtu.be/nd_m17VsRh0?si=af5-8Zi4CawLRL5s
+app = Flask(__name__)
 
-# recebe UMA url de video no youtube e baixa o audio
-def download_audio(ytb_url):    
+# Rota para baixar o áudio do YouTube como MP3
+@app.route('/converter', methods=['POST'])
+def download_audio():
+    ytb_url = request.json.get('ytb_url')  # URL do YouTube passada no corpo da requisição
+    if not ytb_url:
+        return jsonify({"erro": "A URL do YouTube é necessária!"}), 400
 
+    # Decodificar cookies do ambiente
     cookies_base64 = os.environ.get('YOUTUBE_COOKIES')
-
     with open('cookies.txt', 'wb') as f:
         f.write(base64.b64decode(cookies_base64))
     
+    # Opções do yt-dlp
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -22,16 +25,27 @@ def download_audio(ytb_url):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'cookiefile':'cookies.txt',
-        'outtmpl': '/tmp/temp_audio.%(ext)s',  # Salvar o arquivo temporariamente no servidor
+        'cookiefile': 'cookies.txt',
+        'outtmpl': '/tmp/temp_audio.%(ext)s',  # Salvar temporariamente no servidor
     }
+
     try:
+        # Fazer o download e conversão com yt-dlp
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(ytb_url, download=True)
             filename = ydl.prepare_filename(info_dict)
 
+        # Garantir que o arquivo MP3 foi criado
         mp3_path = filename.replace('.webm', '.mp3')
-        return send_file(mp3_path, as_attachment=True, download_name=f"{info_dict['title']}.mp3")
-    except Exception as e:
-        return jsonify({"erro_downloading_conversor":   str(e)}), 500
+        if os.path.exists(mp3_path):
+            # Usar safe_join para evitar problemas de path traversal
+            safe_mp3_path = safe_join('/tmp', os.path.basename(mp3_path))
+            return send_file(safe_mp3_path, as_attachment=True, download_name=f"{info_dict['title']}.mp3", mimetype='audio/mpeg')
+        else:
+            return jsonify({"erro": "Arquivo MP3 não encontrado!"}), 500
 
+    except Exception as e:
+        return jsonify({"erro_downloading_conversor": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
